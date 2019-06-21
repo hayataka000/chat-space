615375b299b3f5f25b168dc58f27db579e1a5d12
hayataka000 <hysdtksg@gmail.com>
2019-06-21 19:04:51 +0900

    create group function 2nd

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,3 +4,11 @@ class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :messages
 end
+
+def show_last_message
+  if (last_message = messages.last).present?
+    last_message.content? ? last_message.content : '画像が投稿されています'
+  else
+    'まだメッセージはありません。'
+  end
+end
