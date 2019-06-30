bb2815c359712e28eee4a9ce6e086722fb5a6b15
hayataka000 <hysdtksg@gmail.com>
2019-06-30 22:02:56 +0900

    自動更新実装

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,11 @@
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Message.where("id > ?", "#{params[:id]}")
+    @messages = Message.all
+    respond_to do |format|
+      format.html
+      format.json { @messages = Message.where('id > ?', params[:id])}
+      # binding.pry
+      # params :messageを消したら動いた！
+    end
   end
-  
 end
